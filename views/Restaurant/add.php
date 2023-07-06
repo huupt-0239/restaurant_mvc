7b4dbbe988cd37f155e850c2828507156db28176
huupt-0239 <pham.tien.huu@sun-asterisk.com>
2023-07-06 23:18:50 +0700

    update add function

--- views/Restaurant/add.php
+++ views/Restaurant/add.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+$validate = $_SESSION['validate'] ?? [];
+$input = $_SESSION['input'] ?? [];
+unset($_SESSION['validate']);
+unset($_SESSION['input']);
+?>
 
 <head>
     <meta charset="UTF-8">
@@ -39,76 +45,40 @@
 </head>
 
 <body>
-    <div class="flex items-center justify-center h-screen">
-        <form class="w-1/3 bg-white rounded shadow p-8" action="?mod=restaurant&act=store" method="post" onsubmit="return validateForm()">
-            <h1 class="text-center text-4xl mb-16">Create Restaurant</h1>
+    <div class="flex items-center justify-center h-screen bg-gradient-to-r from-pink-500 via-rose-500 to-orange-400">
+        <form class="w-1/3 bg-white rounded-lg shadow p-8 space-y-6" action="?mod=restaurant&act=store" method="post">
+            <h1 class="text-center text-4xl font-bold leading-9 tracking-tight text-gray-900 mb-16">Create Restaurant</h1>
             <div class="input-field">
-                <h2>Restaurant Name</h2>
-                <input id="name" class="ml-[81px] mt-3 justify-center rounded border border-gray-300 w-[17rem] px-3 py-2" type="text" name="name" value="">
-                <div id="nameError" class="error-message ml-20 mb-6"></div>
+                <div class="relative">
+                    <input type="text" name="name" id="name" class="block focus:border-rose-500 border-2 px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-rose-500 peer" placeholder=" " value="<?php echo $input['name'] ?? ''; ?>">
+                    <label for="name" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-[0.8] top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-rose-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-[0.8] peer-focus:-translate-y-4 left-1">Restaurant Name</label>
+                </div>
+                <?php if (isset($validate['name'])) : ?>
+                    <p class="text-red-500 text-xs"><?php echo $validate['name']; ?></p>
+                <?php endif; ?>
             </div>
             <div class="input-field">
-                <h2>Description</h2>
-                <input id="description" class="ml-20 mt-3  justify-center rounded border border-gray-300 px-3 w-[17rem] py-2" type="text" name="description" value="">
-                <div id="descriptionError" class="error-message mb-6 ml-20"></div>
+                <div class="relative">
+                    <input type="text" name="description" id="name" class="block focus:border-rose-500 border-2 px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-rose-500 peer" placeholder=" " value="<?php echo $input['description'] ?? ''; ?>">
+                    <label for="description" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-[0.8] top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-rose-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-[0.8] peer-focus:-translate-y-4 left-1">Description</label>
+                </div>
+                <?php if (isset($validate['description'])) : ?>
+                    <p class="text-red-500 text-xs"><?php echo $validate['description']; ?></p>
+                <?php endif; ?>
             </div>
             <div class="input-field">
-                <h2>Image URL</h2>
-                <input id="img_url" class="ml-20 w-[17rem] mt-3 justify-center rounded border border-gray-300 px-3 py-2" type="text" name="img_url" value="">
-                <div id="imgUrlError" class="error-message ml-20 mb-6"></div>
+                <div class="relative">
+                    <input type="text" name="img_url" id="img_url" class="block focus:border-rose-500 border-2 px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-rose-500 peer" placeholder=" " value="<?php echo $input['img_url'] ?? ''; ?>">
+                    <label for="img_url" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-[0.8] top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-rose-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-[0.8] peer-focus:-translate-y-4 left-1">Image URL</label>
+                </div>
+                <?php if (isset($validate['img_url'])) : ?>
+                    <p class="text-red-500 text-xs"><?php echo $validate['img_url']; ?></p>
+                <?php endif; ?>
             </div>
             <div class="flex justify-center">
                 <button class="rounded bg-blue-500 text-white px-6 py-2" type="submit">Save</button>
             </div>
         </form>
     </div>
-
-    <script>
-        function validateForm() {
-            var name = document.getElementById("name").value;
-            var description = document.getElementById("description").value;
-            var img_url = document.getElementById("img_url").value;
-
-            var nameError = document.getElementById("nameError");
-            var descriptionError = document.getElementById("descriptionError");
-            var imgUrlError = document.getElementById("imgUrlError");
-
-            nameError.textContent = "";
-            descriptionError.textContent = "";
-            imgUrlError.textContent = "";
-
-            var hasError = false;
-
-            if (name === "") {
-                nameError.textContent = "Please enter a restaurant name";
-                hasError = true;
-                document.getElementById("name").classList.add("input-error");
-            } else {
-                document.getElementById("name").classList.remove("input-error");
-            }
-
-            if (description === "") {
-                descriptionError.textContent = "Please enter a description";
-                hasError = true;
-                document.getElementById("description").classList.add("input-error");
-            } else {
-                document.getElementById("description").classList.remove("input-error");
-            }
-
-            if (img_url === "") {
-                imgUrlError.textContent = "Please enter an image URL";
-                hasError = true;
-                document.getElementById("img_url").classList.add("input-error");
-            } else {
-                document.getElementById("img_url").classList.remove("input-error");
-            }
-
-            if (hasError) {
-                return false;
-            }
-
-            return true;
-        }
-    </script>
 
 </html>
